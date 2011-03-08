9e3f52d6efba4e99ca548cac5191ca507ccff20c
Debby <Debby@Mail>
2011-03-08 15:27:24 +0100

    Model.cs added with EllipseText property

---
+++ EtwasGrafik/Model.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EtwasGrafik
+{
+    public class Model : INotifyPropertyChanged
+    {
+
+        private string _ellipseText;
+        public string EllipseText
+        {
+            get { return _ellipseText; }
+            set
+            {
+                if(value != _ellipseText)
+                {
+                    _ellipseText = value;
+                    OnPropertyChanged("EllipseText");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string message)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(message));
+            }
+        }
+    }
+}
